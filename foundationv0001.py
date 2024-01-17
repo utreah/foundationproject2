b77ae7f475e9ee5a3df3e1364c1374eac77e029c
utreah <ersoyugur1528@gmail.com>
2024-01-17 21:04:18 +0000

    food_menu has been updated. New images has been added to be resized

--- foundationv0001.py
+++ foundationv0001.py
@@ -4,19 +4,34 @@ import datetime
 
 from time import strftime
 def print_items_in_food_menu():
+    # width = 245px,  height=186px for future references
+
     clear_screen()
     time_on_screen()
+    # Create a frame
+    food_frame = tk.Frame(main_pos_name)
+    food_frame.pack(fill=tk.BOTH, expand=True)
 
-    test_food_frame = ttk.Frame(main_pos_name)
-    test_food_frame.pack()
+    # Create a button and add food_kebab.png
+    food_kebab_image = tk.PhotoImage(file="images/food/food_kebab.png")
+    kebab_button = tk.Button(food_frame, image=food_kebab_image)
+    kebab_button.image = food_kebab_image
+    kebab_button.place(x=10, y=5)
 
-    # Use PhotoImage to load the image
-    test_button_image = tk.PhotoImage(file="images/kebab.png")
+    # Create label for dish feature(s) -> Name, price, V/VE/GF
+    kebab_dish_information = tk.Label(food_frame, text="Name: Lamb Kebab \nPrice(S/L): 10.99£/15.99£")
+    kebab_dish_information.place(x=75, y=200)
 
-    # Create a button and place the image on it
-    test_button = ttk.Button(test_food_frame, image=test_button_image)
-    test_button.image = test_button_image
-    test_button.pack(side=tk.LEFT,padx=5,pady=5)
+    # Create a button and add food_lahmacun.png
+    food_lahmacun_image = tk.PhotoImage(file="images/food/food_lahmacun.png")
+    lahmacun_button = tk.Button(food_frame, image=food_lahmacun_image)
+    lahmacun_button.image = food_lahmacun_image
+    lahmacun_button.place(x=300, y=5)
+
+    # Create label for dish feature(s) -> Name, price, V/VE/GF
+    lahmacun_dish_information = tk.Label(food_frame, text="Name: Lahmacun \nPrice(S/L): 3.99£/5.99£ \nV/VE option available!")
+    lahmacun_dish_information.place(x=365, y=200)
+
 
 
 
@@ -44,6 +59,7 @@ def get_computer_time_date():
     time_label_top_left_corner.after(1000,get_computer_time_date)
 
 def main_menu():
+    clear_screen()
     time_on_screen()
 
     # Create a BUTTON SECTION frame
@@ -74,7 +90,9 @@ def main_menu():
     coffee_button = tk.Button(button_section_frame, text="COFFEE", bg="BLACK", font=('bold'), fg="WHITE")
     coffee_button.place(x=550, y=300, width=200, height=200)
 
-
+def go_back_to_main_menu():
+    another_test_button = tk.Button(main_pos_name, text="BACK TO MENU",command=main_menu, bg="RED",fg="WHITE", font=('Arial', 18, 'bold'))
+    another_test_button.place(x=10, y=800, width=200, height=200)
 
 def clear_screen():
     for widgets in main_pos_name.winfo_children():
@@ -95,8 +113,8 @@ main_pos_name.geometry(f"{get_screen_res_width}x{get_screen_res_height-72}+{set_
 
 
 
-
-test_button = tk.Button(main_pos_name, text="Test Button", command=main_menu).pack()
+main_menu()
+#test_button = tk.Button(main_pos_name, text="Test Button", command=main_menu).pack()
 
 
 
