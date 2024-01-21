6296b4e3df60b4e78fc6d779be918de7ce39776d
utreah <ersoyugur1528@gmail.com>
2024-01-21 19:01:19 +0000

    Treeview has been updated and now working.

--- test.py
+++ test.py
@@ -4,43 +4,49 @@ from tkinter import ttk
 main_pos_name = tk.Tk()
 
 
-seenProduct = set()
-
-test_tuple_v2 = []
-
 
 # Define treeview identifiers
-treeview_customer_basket_identifier_columns = ('product_name', 'product_quantity', 'product_size', 'product_price')
+treeview_customer_basket_identifier_columns = ('product_name', 'product_size', 'product_quantity', 'product_price')
 # Create a treeview
 customer_basket_treeview = ttk.Treeview(main_pos_name, columns=treeview_customer_basket_identifier_columns, show='headings')
 # Create headings and place treeview onto frame 'button_section_frame'
 customer_basket_treeview.heading('product_name', text='Product Name')
-customer_basket_treeview.heading('product_quantity', text='Product Quantity')
 customer_basket_treeview.heading('product_size', text='Product Size')
+customer_basket_treeview.heading('product_quantity', text='Product Quantity')
 customer_basket_treeview.heading('product_price', text='Product Price')
 customer_basket_treeview.pack()
 # Create a tuple and append product information
-test_tuple = []
-product_name = ["test1", "test2", "test3", "test1", "test1", "test2", "test3"]
+
+# ARRAYS
+product_name = ["test1", "test2", "test3", "test1", "test1", "test2", "test3","TEST1"]
 product_quantity = ["1", "2", "3", "1", "1", "2", "3"]
-product_size = ["S", "L", "S", "S", "S", "L", "S"]
-product_price = ["1", "2", "3", "1", "1", "2", "3"]
-for productCounter in product_name:
-    if productCounter not in seenProduct:
-        count = product_name.count(productCounter)
-        test_tuple_v2.append((f'{productCounter}', f'{count}'))
-        seenProduct.add(productCounter)
-        print(productCounter, count)
-        test_tuple.append((f'{productCounter}', f'{count}' ))  
-print(test_tuple_v2)
+product_size = ["S", "L", "S", "S", "S", "L", "S","L"]
+product_price = ["1", "2", "5", "1", "1", "2", "5","8"]
+product_mix_name_size = []
+test_seen = set()
+#for i in range(0,len(product_name)):
+#    product_mix_name_size.append((product_name[i],product_size[i]))
+def delete():
+    for item in customer_basket_treeview.get_children():
+        customer_basket_treeview.delete(item)
+def find_food_price(productName):
+    get_index = product_name.index(productName)
+    return product_price[get_index]
+def add():
+    for i,k in zip(product_name, range(0, len(product_name))):
+        if i not in test_seen:
+            foodPrice = find_food_price(i)
+            count = product_name.count(i)
+            product_mix_name_size.append((product_name[k], product_size[k], count,foodPrice))
+            test_seen.add(i)
+    #print("First append: ", product_mix_name_size)
+    # [('test1', 'S'), ('test2', 'L'), ('test3', 'S'), ('test1', 'S'), ('test1', 'S'), ('test2', 'L'), ('test3', 'S'), ('TEST1', 'L')]
+    # 3 adet TEST1 S || 2 ADET TEST2 L || 2 ADET TEST3 S || 1 adet TEST1 L
+    #print("After count append:", product_mix_name_size)
 
-for i in range(len(product_name)):
-   test_tuple.append((f'{product_size[i]}', f'{product_price[i]}'))
-
-for appendToTreeview in test_tuple:
-   customer_basket_treeview.insert('', tk.END, values=appendToTreeview)
-
-
-
-
+    print("Last append: ", product_mix_name_size)
+    for testings in product_mix_name_size:
+        customer_basket_treeview.insert('', tk.END,values=testings)
+test_button = tk.Button(main_pos_name, text="Test Button", command=delete).pack()
+test_button_v2 = tk.Button(main_pos_name, text="Add button", command = add).pack()
 main_pos_name.mainloop()
